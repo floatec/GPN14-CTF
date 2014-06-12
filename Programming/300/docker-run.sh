85fb6cccc3f976818820a1d22a3073c44f415b4f
MrDisconnect <sp@shadex.net>
2014-06-12 20:34:01 +0200

    Docker Prog 300

---
+++ Programming/300/docker-run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# `/sbin/setuser memcache` runs the given command as the user `memcache`.
+# If you omit that part, the command will be run as root.
+cd /opt/ctf
+exec /sbin/setuser ctf /usr/bin/env python ./superbrain.py 2>&1
