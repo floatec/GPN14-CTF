cf857a20bb41d3dd92a1563f5651960abaa97dd8
Stephan <sp@shadex.net>
2014-06-12 11:47:46 +0200

    Trivia 300 update, first docker test

--- Trivia/300/server.py
+++ Trivia/300/server.py
@@ -2,7 +2,11 @@
 #easy_install SimpleCV
 
 import time
+import os
+import hashlib
 from SimpleCV import Image, Camera, Display
+
+SAVE_TO = "./imgs"
 
 display = Display((800,600))
 cam = Camera()
@@ -13,8 +17,15 @@ while True:
 	barcode = img.findBarcode()
 	if barcode:
 		data = str(barcode[0].data)
+		#save image for projection
+		img.save(os.path.join(SAVE_TO, hashlib.md5(str(time.time())).hexdigest()))
+
+		#show recognition
 		barcode.draw()
 		img.drawText("\'%s\' detected." % data, 10, 10, fontsize=30)
+
 		#activate challenge here
+
+	#show on screen
 	img.save(display)
 	time.sleep(1.0)
