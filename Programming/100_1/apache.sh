d640a4d3919eace7b9e645087a042fd24847610f
MrDisconnect <sp@shadex.net>
2014-06-12 16:12:42 +0200

    Add docker config for Prog100_1/200

--- Programming/100_1/apache.sh
+++ Programming/100_1/apache.sh
@@ -2,5 +2,5 @@
 # `/sbin/setuser memcache` runs the given command as the user `memcache`.
 # If you omit that part, the command will be run as root.
 /etc/init.d/apache2 start
-pid=`cat /var/run/apache2/apache2.pid
+pid=`cat /var/run/apache2/apache2.pid`
 while kill -0 $pid > /dev/null; do sleep 1; done
